151171ab6feb495c499172c6e0ad56d70d4f0e91
willymwaura <wilsonmwaura697@gmail.com>
2023-02-16 00:23:53 +0300

    requirements.txt

--- dict/settings.py
+++ dict/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-63q9h(znz#1e)m&74w9#_n5nakdb!qx*fqhfi=&k7e3bwmaia$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",".vercel.app"]
 CORS_ALLOWED_ORIGINS=["*"]
 
 
